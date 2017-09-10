3c7c2d35dfdb6e73e7d43930fbcbeec054acf9f7
Lloyd Feng <fjch1997@outlook.com>
2017-09-09 23:03:30 -0400

    Done

--- Arduino/ArduinoZero/ArduinoZero.ino
+++ Arduino/ArduinoZero/ArduinoZero.ino
@@ -1,19 +1,107 @@
+//Arthurs: Jingchao Feng, Sanjit Sighn
+
 #include <Keyboard.h>
-const int Button = 2;
+
+/*
+Rotary Scan codes
+First Digit: Alway 2
+Second Digit: Analog Pin Number
+Third Digit: 0 down 1 up
+Example: 201 means pin 0 turned up
+		 220 means pin 2 turned down
+*/
+
+/*
+Button Scan codes
+First Digit: Always 2
+Second and Third Digit: pin number
+Example: 213 means pin 13 is pressed
+		 203 means pin 3 is pressed
+*/
+
+const int rotarySensorInterval = 100;
+const int scanCodeStartPoint = 200;
+
+int rotarySensorValues[5];
+bool lastButtonSensorStates[14];
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(Button, INPUT);
-  pinMode(A0, INPUT);
-  Serial.begin(9600);
+	// put your setup code here, to run once:
+	pinMode(A0, INPUT);
+	pinMode(A1, INPUT);
+	pinMode(A2, INPUT);
+	pinMode(A3, INPUT);
+	pinMode(A4, INPUT);
+	pinMode(A5, INPUT);
+	for (size_t i = 0; i < 14; i++)
+	{
+		pinMode(i, INPUT);
+	}
+
+	Serial.begin(9600);
+	Serial.write("Setup Complete");
+	Keyboard.begin();
+	for (uint32_t i = 2; i < 5; i++)
+	{
+		rotarySensorValues[i] = analogRead(i) / rotarySensorInterval;
+	}
+	for (size_t i = 0; i < 14; i++)
+	{
+		lastButtonSensorStates[i] = false;
+	}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.write("DEBUG");
-  if (digitalRead(Button) == HIGH)
-  {
-    Serial.write(analogRead(A0));
-  }
-  delay(500);
+	// Serial.write("Loop executing.");
+	doAnalog();
+	doDigital();
+	delay(1);
+}
+
+void doAnalog() {
+	//TODO i < 6
+	for (size_t i = 0; i < 1; i++)
+	{
+		uint32_t value = analogRead(i);
+		//Serial.println("Pin " + String(i) + " value is " + String(value));
+		int level = value / rotarySensorInterval;
+		//Serial.println("Pin " + String(i) + " level is " + String(level));
+		int timesToPress = rotarySensorValues[i] - level;
+		//Serial.println("Pin " + String(i) + " timesToPress is " + String(timesToPress));
+		rotarySensorValues[i] = level;
+		int upOrDown = timesToPress > 0;
+		//Serial.println("Pin " + String(i) + " state is " + String(upOrDown));
+		uint8_t scanCode = scanCodeStartPoint;
+		scanCode += (i * 10);
+		scanCode += upOrDown;
+		for (size_t j = 0; j < abs(timesToPress); j++)
+		{
+			//Serial.println("Sending Scancode " + String(scanCode));
+			Keyboard.press(scanCode);
+			Keyboard.release(scanCode);
+		}
+	}
+}
+
+void doDigital() {
+	for (size_t i = 2; i < 14; i++)
+	{
+		bool result;
+		if (digitalRead(i) == HIGH)
+			result = true;
+		else
+			result = false;
+		Serial.println("Button " + String(i) + " state is " + result);
+		if (result != lastButtonSensorStates[i])
+		{
+			uint8_t scanCode = scanCodeStartPoint;
+			scanCode += i;
+			if (result) {
+				Serial.println("Sending Scancode " + String(scanCode));
+				Keyboard.press(scanCode);
+				Keyboard.release(scanCode);
+			}
+			lastButtonSensorStates[i] = result;
+		}
+	}
 }
